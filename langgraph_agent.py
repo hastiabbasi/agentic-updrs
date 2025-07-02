16e7eca2c2cee27ae77722cb31f99838e497cb84
Hasti Abbasi Kenarsari <hastiabbasikenarsari@gmail.com>
2025-07-02 15:32:44 -0400

    added extract pose node tool

--- langgraph_agent.py
+++ langgraph_agent.py
@@ -4,6 +4,9 @@ from langchain.tools import tool
 from langchain_google_genai import ChatGoogleGenerativeAI
 import numpy as np 
 
+# pose extraction tool 
+from utils.pose_utils import extract_keypoints
+
 llm = ChatGoogleGenerativeAI(
     model="gemini-2.0-flash",
     temperature=0,
@@ -17,3 +20,8 @@ class GraphState(TypedDict):
     velocity_data: Optional[Dict[str, float]]
     tremor_data: Optional[Dict[str, float]]
     tremor_data: Optional[Dict[str, Any]]
+
+@tool 
+def extract_pose_node(state: GraphState) -> GraphState:
+    pose = extract_keypoints(state['video_path'], joints=["RIGHT_INDEX", "RIGHT_THUMB"])
+    return {**state, "pose_data": pose}

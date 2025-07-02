08e55fb00ac73c87ecaba7a5e6454486e972c36c
Hasti Abbasi Kenarsari <hastiabbasikenarsari@gmail.com>
2025-07-02 15:08:10 -0400

    added setup for Gemini LLM (without the api key ?)

--- langgraph_agent.py
+++ langgraph_agent.py
@@ -3,3 +3,9 @@ from langgraph.graph import StateGraph, END
 from langchain.tools import tool 
 from langchain_google_genai import ChatGoogleGenerativeAI
 import numpy as np 
+
+llm = ChatGoogleGenerativeAI(
+    model="gemini-2.0-flash",
+    temperature=0,
+    convert_system_message_to_human=True
+)

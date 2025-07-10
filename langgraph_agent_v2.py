a3dee24268a3aba07e62a3a55ebaf9573ea6ce4f
Hasti Abbasi Kenarsari <hastiabbasikenarsari@gmail.com>
2025-07-10 11:11:31 -0400

    changed input block to force it to use get_pose_data

--- langgraph_agent_v2.py
+++ langgraph_agent_v2.py
@@ -139,7 +139,7 @@ if __name__ == "__main__":
     # inputs = {"messages": [HumanMessage(content=prompt)]}
     inputs = {
         "messages": [
-            HumanMessage(content=prompt),
+            HumanMessage(content='Use get_pose_data with video_path="/Users/hastiabbasi/agentic-updrs/agentic-updrs/FT_vids/sub1vid7.mp4"'),
         ]
     }
 

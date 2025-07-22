1ed0684f9b21de2fa1257a60033cb2e5a86e8e81
Hasti Abbasi Kenarsari <hastiabbasikenarsari@gmail.com>
2025-07-22 14:58:34 -0400

    build: fixed imports

--- examples/gemini_chain_test.py
+++ examples/gemini_chain_test.py
@@ -4,7 +4,7 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.tools import tool 
 from langchain_core.messages import HumanMessage, ToolMessage, BaseMessage
 from pydantic import BaseModel, Field
-from typing import Dict
+from typing import Dict, List 
 
 load_dotenv()
 assert os.getenv("GOOGLE_API_KEY"), "GOOGLE_API_KEY not set in environment."
